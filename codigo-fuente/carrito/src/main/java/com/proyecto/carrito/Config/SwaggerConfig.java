b644ed67954b4f41493c703ec12ef5b381cf1de5
Cristobal Hermosilla <cr.hermosillaa@duocuc.cl>
2026-06-14 21:27:18 +0000

    doc: funcionamiento Swagger

---
+++ codigo-fuente/carrito/src/main/java/com/proyecto/carrito/Config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.proyecto.carrito.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Carrito API")
+                        .version("1.0")
+                        .description("API para gestionar el carrito de compras"));
+    }
+}
